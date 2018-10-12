d4dab9d54e5a1769801785e744ce5ff4731ce21b
Shuhratzhan Nizamov <nshuhratjan@mail.ru>
2018-10-13 01:03:02 +0600

    Customer Repository created

---
+++ LibraryManagment/Data/Repository/CustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagment.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagment.Data.Repository
+{
+    public class CustomerRepository : Repository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
