6a296e405da685819cb8c33dfa146daa3e63cb64
Shuhratzhan Nizamov <nshuhratjan@mail.ru>
2018-10-13 00:47:32 +0600

    Book Repository Finished

--- LibraryManagment/Data/Repository/BookRepository.cs
+++ LibraryManagment/Data/Repository/BookRepository.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using LibraryManagment.Data.Interfaces;
+using LibraryManagment.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LibraryManagment.Data.Repository
 {
-    public class BookRepository
+    public class BookRepository : Repository<Book>, IBookRepository
     {
+        public BookRepository(ApplicationDbContext context):base(context)
+        {
+        }
+        public IEnumerable<Book> FindWithAuthor(Func<Book, bool> predicate)
+        {
+            return _context.Books
+                .Include(a => a.Author)
+                .Where(predicate);
+        }
+
+        public IEnumerable<Book> FindWithAuthorAndBorrower(Func<Book, bool> predicate)
+        {
+            return _context.Books
+                .Include(a => a.Author)
+                .Include(a => a.Borrower)
+                .Where(predicate);
+        }
+
+        public IEnumerable<Book> GetAllWithAuthor()
+        {
+            return _context.Books.Include(a => a.Author);
+        }
     }
 }
